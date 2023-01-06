21a50854989189e9318819b990189b6948b12500
Shan Pisciotta <shanpisciotta.pro@gmail.com>
2023-01-06 19:49:03 +0100

    Duplication des collapses

--- src/components/Collapse/Collapse.js
+++ src/components/Collapse/Collapse.js
@@ -3,94 +3,42 @@ import open from "../../assets/icons/open.svg";
 import close from "../../assets/icons/close.svg";
 import style from "./Collapse.module.css";
 
-const Collapse = () => {
-  const [isOpen, setIsOpen] = useState(open);
+const Collapse = ({children}) => {
+  const [isOpen, setIsOpen] = useState(false);
 
   const handleClick = () => {
-    if (isOpen === open) {
-      setIsOpen(close);
-    } else if (isOpen === close) {
-      setIsOpen(open);
+    if (!isOpen) {
+      setIsOpen(true);
+    } else if (isOpen) {
+      setIsOpen(false);
     }
   };
 
   return (
     <div className={style["collapse-container"]}>
-      <div className={style["collapse-background"]}>
-        <h3>Fiabilité</h3>
-        <img
-          src={isOpen}
-          alt="Ouvrir"
-          className={style.open}
-          onClick={() => handleClick()}
-        />
-      </div>
-      {isOpen === close ? (
-        <div className={style["description-container"]}>
-          <p>
-            Les annonces postées sur Kasa garantissent une fiabilité totale. Les
-            photos sont conformes aux logements, et toutes les informations sont
-            régulièrement vérifiées par nos équipes.
-          </p>
+      <div className={style["collapse-content"]}>
+        <div className={style["collapse-background"]} onClick={() => handleClick()}>
+          {children}
+          {isOpen ? (
+            <img
+              src={close}
+              alt="Fermer"
+              className={style.open}
+            />
+          ) : (
+            <img
+              src={open}
+              alt="Ouvrir"
+              className={style.open}
+            />
+          )}
         </div>
-      ) : null}
-      <div className={style["collapse-background"]}>
-        <h3>Respect</h3>
-        <img
-          src={isOpen}
-          alt="Ouvrir"
-          className={style.open}
-          onClick={() => handleClick()}
-        />
+        {isOpen ? (
+          <div className={style["description-container"]}>
+            {children}
+          </div>
+        ) : null}
       </div>
-      {isOpen === close ? (
-        <div className={style["description-container"]}>
-          <p>
-            La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
-            comportement discriminatoire ou de perturbation du voisinage
-            entraînera une exclusion de notre plateforme.
-          </p>
-        </div>
-      ) : null}
-      <div className={style["collapse-background"]}>
-        <h3>Service</h3>
-        <img
-          src={isOpen}
-          alt="Ouvrir"
-          className={style.open}
-          onClick={() => handleClick()}
-        />
-      </div>
-      {isOpen === close ? (
-        <div className={style["description-container"]}>
-          <p>
-            Nos équipes se tiennent à votre disposition pour vous fournir une
-            expérience parfaite. N'hésitez pas à nous contacter si vous avez la
-            moindre question.
-          </p>
-        </div>
-      ) : null}
-      <div className={style["collapse-background"]}>
-        <h3>Responsabilité</h3>
-        <img
-          src={isOpen}
-          alt="Ouvrir"
-          className={style.open}
-          onClick={() => handleClick()}
-        />
-      </div>
-      {isOpen === close ? (
-        <div className={style["description-container"]}>
-          <p>
-            La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que
-            pour les voyageurs, chaque logement correspond aux critères de
-            sécurité établis par nos services. En laissant une note aussi bien à
-            l'hôte qu'au locataire, cela permet à nos équipes de vérifier que
-            les standards sont bien respectés. Nous organisons également des
-            ateliers sur la sécurité domestique pour nos hôtes.
-          </p>
-        </div>
-      ) : null}
     </div>
   );
 };
--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,15 +1,26 @@
-import React from 'react';
-import Banner from '../../components/Banner/Banner';
+import React from "react";
+import Banner from "../../components/Banner/Banner";
 import background from "../../assets/images/about-background.png";
-import Collapse from '../../components/Collapse/Collapse';
+import Collapse from "../../components/Collapse/Collapse";
 
 const About = () => {
-    return (
-        <div>
-            <Banner background={background} />
-            <Collapse />
-        </div>
-    );
+  return (
+    <div>
+      <Banner background={background} />
+      <Collapse>
+        <h3>Fiabilité</h3>
+      </Collapse>
+      <Collapse>
+        <h3>Respect</h3>
+      </Collapse>
+      <Collapse>
+        <h3>Service</h3>
+      </Collapse>
+      <Collapse>
+        <h3>Sécurité</h3>
+      </Collapse>
+    </div>
+  );
 };
 
 export default About;
